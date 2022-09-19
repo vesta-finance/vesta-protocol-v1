57b3f4dfb41b04ae6f05e33028b5e988bcbf3fd5
Atum <0xAtum@protonmail.com>
2022-09-19 13:19:44 -0400

    return to 0.8.10

--- hardhat.config.js
+++ hardhat.config.js
@@ -49,18 +49,6 @@ module.exports = {
         version: "0.8.2",
       },
     ],
-    overrides: {
-      "contracts/TroveRedemptor.sol": {
-        version: "0.8.17",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          },
-          viaIR: true,
-        }
-      }
-    }
   },
   networks: {
     hardhat: {
